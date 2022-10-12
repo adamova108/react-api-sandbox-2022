c28251538f0c2620ce0c38a1dadfe9ab40594f03
Adam Luzsi <luzsiadam@gmail.com>
2022-10-12 10:26:23 +0200

    + Updated App

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,30 @@
 import express from "express"
+import cors from 'cors'
 import { authRoutes } from "./routes/auth"
 import { mainRoutes } from "./routes/main"
 
+// DI 
+import { ClientInvoicesRepoAggregate } from './repositories/clientInvoicesRepoAggregate'
+import { ClientsRepository } from './repositories/clientsRepository'
+import { InvoicesRepository } from './repositories/invoicesRepository'
+
+
 // export needed for unit/integration testing
 export const app = express()
+
+app.use(cors())
 
 // parse JSON when content type is application/json
 app.use(express.json())
 
 authRoutes(app)
 mainRoutes(app)
+
+export const setup = async () => {
+    const invoiceRepo = await InvoicesRepository.getInstance();
+    app.set("invoicesRepo", invoiceRepo);
+    const invoiceClientAggregate = await ClientInvoicesRepoAggregate.getInstance();
+    app.set("invoiceClientAggregate", invoiceClientAggregate);
+    const clientsRepo = await ClientsRepository.getInstance();
+    app.set("clientsRepo", clientsRepo);
+}
