ac50d90a1061bce6b82e782065a6bcb22c28ff7f
Adam Luzsi <luzsiadam@gmail.com>
2022-11-10 12:06:38 +0100

    + Added getInstance for singleton

--- src/repositories/usersRepository.ts
+++ src/repositories/usersRepository.ts
@@ -1,14 +1,26 @@
+import { jsonDir } from '../config';
 import { BaseRepository } from './baseRepository';
 import { CompanyDetails } from './company';
 
 type UserData = {
     id: string
+    name: string
     email: string
     password: string
     companyDetails?: CompanyDetails
 };
 
 export class UsersRepository extends BaseRepository<UserData> {
+    private static _instance: UsersRepository;
+    
+    static async getInstance () {
+        if (!UsersRepository._instance) {
+            UsersRepository._instance = new UsersRepository();
+            await UsersRepository._instance.init(jsonDir);
+        }
+        return UsersRepository._instance;
+    }
+    
     constructor () {
         super("users.json");
     }
